829799e6fa4c36db2c61e46a01bda36f73f2022b
sians5544 <tocic@naver.com>
2021-11-04 10:10:47 +0900

    algorithm: leetcode 921

---
+++ algorithm/leetcode/921.minimum add to make valid.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var minAddToMakeValid = function(s) {
+    
+  let stack = [];
+  
+  let answer = 0;
+  
+  for(let i = 0; i < s.length; i++){
+      
+      if(s[i] !== '(' && stack.length){
+      
+          stack.pop();
+      }
+      else if(s[i] === ')' && stack.length ==0){
+          
+          answer++;
+      }
+      else{
+          
+          stack.push(s[i]);
+      }
+      
+  }
+  
+  
+  answer+= stack.length;
+  
+  return answer;
+  
+};
