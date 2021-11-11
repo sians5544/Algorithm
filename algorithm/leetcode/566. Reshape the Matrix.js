2d7b5a3610643e4a4ae472f56961b869838a2de2
sians5544 <tocic@naver.com>
2021-11-11 11:23:21 +0900

    leetcode 566 solved

---
+++ algorithm/leetcode/566. Reshape the Matrix.js
@@ -0,0 +1,20 @@
+function solution(mat, r, c) {
+
+  let n = mat[0].length;
+  let answer = Array.from(Array(r), () => Array(c).fill(0));
+
+  if (r * c !== mat.length * mat[0].length) return mat;
+
+  for (let i = 0; i < r; i++) {
+    for (let j = 0; j < c; j++) {
+      let k = c * i + j;
+      let m = Math.floor(k / n);
+      answer[i][j] = mat[m][k % n];
+    }
+  }
+
+  return answer;
+}
+
+console.log(solution([[1, 2], [3, 4]], 1, 4));
+console.log(solution([[1, 2], [3, 4]], 2, 4));
