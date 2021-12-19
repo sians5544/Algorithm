e942d77113fb1f77614586d678546e6a0742b6a0
sians5544 <tocic@naver.com>
2021-12-19 23:03:01 +0900

    우테코 07

--- algorithm/파이널 테스트 대비 복습/우테코07.js
+++ algorithm/파이널 테스트 대비 복습/우테코07.js
@@ -60,21 +60,43 @@ function rotate(grid, n) {
   return result;
 }
 
-// for (int i = 0; i < rotate.length; i++) {
-//   for (int j = 0; j < rotate[i].length; j++) {
-//       switch (degree) {
-//           case 90:
-//               rotate[i][j] = arr[n-1-j][i];
-//               break;
-//           case 180:
-//               rotate[i][j] = arr[n-1-i][m-1-j];
-//               break;
-//           case 270:
-//               rotate[i][j] = arr[j][m-1-i];
-//               break;
-//       }
-//   }
-// }
+function solution(grid, clockwise) {
+  let newGrid = Array.from({ length: grid.length }, (_, i) => Array(i * 2 + 1).fill(''));
+
+  let x, y;
+  for (let i = 0; i < grid.length; i++) {
+    const gridSize = grid[i].length;
+    if (clockwise === true) {
+      x = grid.length - i - 1;
+      y = x * 2;
+    }
+    if (clockwise === false) {
+      x = grid.length - 1;
+      y = i * 2;
+    }
+    for (let j = 0; j < gridSize; j++) {
+      newGrid[x][y] = grid[i][j];
+      if (clockwise === true) {
+        if (j === 0) {
+          x++;
+          y++;
+          continue;
+        }
+        if (j % 2 === 1) y--;
+        if (j % 2 === 0) {
+          x++;
+          y++;
+        }
+      }
+      if (clockwise === false) {
+        if (j % 2 === 1) x--;
+        if (j % 2 === 0) y--;
+      }
+    }
+  }
+  const result = newGrid.map((value) => value.join(''));
+  return result;
+}
 
 // console.log(rotate(['1', '234', '56789'], 3));
 //console.log(rotate(['A', 'MAN', 'DRINK', 'WATER11'], 4));
