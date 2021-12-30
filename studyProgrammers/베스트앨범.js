5149583952bf9dad38ae64574151196c843acbd3
sians5544 <tocic@naver.com>
2021-12-30 21:32:38 +0900

    프로그래머스 베스트 앨범 solved

--- studyProgrammers/베스트앨범.js
+++ studyProgrammers/베스트앨범.js
@@ -1,21 +1,38 @@
 function solution(genres, plays) {
-  let answer = [];
+  var answer = [];
   let hash = new Map();
 
+  let hashsum = new Map();
+
   for (let i = 0; i < genres.length; i++) {
-    if (hash.has(genres[i])) {
-      let list = hash.get(genres[i]);
-      list.push([i, plays[i]]);
-      hash.set(genres[i], list);
-    } else {
-      hash.set(genres[i], [[i, plays[i]]]);
-    }
+    hashsum.set(genres[i], (hashsum.get(genres[i]) || 0) + plays[i]);
   }
 
-  let sortArray = [...hash.values()];
+  let maxMusic = [...hashsum].sort((a, b) => b[1] - a[1]);
 
-  
+  for (let i = 0; i < genres.length; i++) {
+    if (!hash.has(genres[i])) hash.set(genres[i], []);
+    let hashArr = hash.get(genres[i]);
+    hashArr.push([i, plays[i]]);
+    hash.set(genres[i], hashArr);
+  }
 
+  // firstMusic.slice(0, 2);
+
+  while (maxMusic.length > 0) {
+    let firstMusic = [...hash.get(maxMusic[0][0])].sort((a, b) =>
+      a[1] === b[1] ? a[0] - b[0] : b[1] - a[1]
+    );
+    firstMusic.slice(0, 2).map((value) => answer.push(value[0]));
+    maxMusic.shift();
+  }
+
+  let hasgValue = [...hash.values()];
+
+  // for (let hg of hasgValue) {
+  //   hg.sort((a, b) => (a[1] === b[1] ? a[0] - b[0] : b[1] - a[1]));
+  //   answer.push();
+  // }
   return answer;
 }
 
