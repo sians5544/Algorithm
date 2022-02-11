0668ac5d4c8c7474174cc37812aac6d0c3202f52
sians5544 <tocic@naver.com>
2022-02-11 21:29:28 +0900

    boj 15565 solved

---
+++ algorithm/Boj/15565.귀여운라이언.js
@@ -0,0 +1,33 @@
+const { Hash, Cipher } = require('crypto');
+const fs = require('fs');
+const { isGeneratorFunction } = require('util/types');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().trim().split('\n');
+
+let [n, k] = input[0].split(' ').map(Number);
+
+let nums = input[1].split(' ').map(Number);
+
+let cnt = 0;
+let left = 0;
+let answer = 1000001;
+let check = false;
+
+if (k > n) return console.log(-1);
+
+for (let right = 0; right < nums.length; right++) {
+  if (nums[right] === 1) cnt++;
+
+  while (k <= cnt) {
+    check = true;
+
+    if (cnt === k) answer = Math.min(answer, right - left + 1);
+
+    if (nums[left] === 1) {
+      cnt--;
+    }
+    left++;
+  }
+}
+
+console.log(check ? answer : -1);
