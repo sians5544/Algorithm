ab3d2b81f137cac3eb44b8bb4bd1830205b65a84
sians5544 <tocic@naver.com>
2022-04-02 22:56:31 +0900

    0215 test2 solved

--- zero_base_class/0215테스트/문제 4.js
+++ zero_base_class/0215테스트/문제 4.js
@@ -1,18 +1,35 @@
-function solution(n) {
+function solution(nums) {
   let answer = [];
-  let part = [];
-  function DFS(L) {
-    if (L === n + 1) {
-      if (part.length !== 0) answer.push(part.slice());
-    } else {
-      part.push(L);
-      DFS(L + 1);
-      part.pop();
-      DFS(L + 1);
+  let tmp;
+  for (let n of nums) {
+    n--;
+    let t = 1;
+    for (let i = 1; ; i++) {
+      if (n >= t * 9) n -= t * 9;
+      else {
+        tmp = parseInt((i + 1) / 2);
+        let res = Array(100).fill(0);
+        let pal = '';
+        res[0] = 1;
+        for (let j = 0; j < tmp; j++) {
+          res[j] += parseInt(n / t);
+          pal += res[j];
+          n %= t;
+          t /= 10;
+        }
+        for (let j = tmp - (i % 2) - 1; j >= 0; j--) {
+          pal += res[j];
+        }
+        answer.push(parseInt(pal));
+        break;
+      }
+      if (i % 2 === 0) t *= 10;
     }
   }
-  DFS(1);
   return answer;
 }
-
-console.log(solution(3));
+console.log(solution([1, 12, 24]));
+console.log(solution([10, 15, 30]));
+console.log(solution([345, 3456, 2352, 939595]));
+console.log(solution([345, 3456, 2352, 2326, 138748, 395802, 930595]));
+console.log(solution([643809, 968068, 287576, 798592, 138749, 395802, 939595, 958085]));
