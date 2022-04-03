73541e6097add2cf370482aa60a138105756feda
sians5544 <tocic@naver.com>
2022-04-03 22:54:31 +0900

    0215 test 03 solved

--- zero_base_class/0215테스트/문제3.js
+++ zero_base_class/0215테스트/문제3.js
@@ -1,23 +1,27 @@
 function solution(enter, leave) {
-  let answer = {};
-  let room = {};
-
-  enter.forEach((person) => {
-    room[person] = 0;
-    const roomSize = Object.keys(room).length;
-
-    if (roomSize !== 1) {
-      for (const key in room) {
-        room[key] === 0 ? (room[key] = roomSize - 1) : room[key]++;
+  let n = enter.length;
+  for (let i = 0; i < n; i++) enter[i]--;
+  for (let i = 0; i < n; i++) leave[i]--;
+  let answer = Array.from(Array(n), () => 0);
+  let enterIndex = Array.from(Array(n), () => 0);
+  for (let i = 0; i < n; i++) enterIndex[enter[i]] = i;
+  let enterAt = Array.from(Array(n), () => 0);
+  let leaveAt = Array.from(Array(n), () => 0);
+  for (let i = 0, j = 0, maxEnterAt = 0, cnt = 0; i < n; i++) {
+    enterAt[enter[i]] = cnt++;
+    while (j < n && Math.max(maxEnterAt, enterIndex[leave[j]]) === i) {
+      maxEnterAt = Math.max(maxEnterAt, enterIndex[leave[j]]);
+      leaveAt[leave[j]] = cnt++;
+      j++;
+    }
+  }
+  for (let i = 0; i < n; i++)
+    for (let j = i + 1; j < n; j++) {
+      if (!(leaveAt[i] < enterAt[j] || leaveAt[j] < enterAt[i])) {
+        answer[i]++;
+        answer[j]++;
       }
     }
-
-    while (room.hasOwnProperty(leave[0])) {
-      answer[leave[0]] = room[leave[0]];
-      delete room[leave[0]];
-      leave.shift();
-    }
-  });
-
-  return Object.values(answer);
+  return answer;
 }
+console.log(solution([1, 4, 2, 3], [2, 1, 3, 4])); //2 2 1 3
