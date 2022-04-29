d6912fe4a359a204fc521b90ac931d6c2729b6ee
sians5544 <tocic@naver.com>
2022-04-29 20:31:41 +0900

    leetcode 815 solved

---
+++ zero_base_class/leetcode/815.bus Routes.js
@@ -0,0 +1,52 @@
+/**
+ * @param {number[][]} routes
+ * @param {number} source
+ * @param {number} target
+ * @return {number}
+ */
+var numBusesToDestination = function (routes, source, target) {
+  let answer = -1;
+  let L = 0;
+  let flag = false;
+  let hash = {};
+  let visited = Array(routes.length).fill(0);
+  let queue = [];
+  let buslines = [];
+
+  if (source === target) return 0;
+
+  for (let i = 0; i < routes.length; i++) {
+    for (let j = 0; j < routes[i].length; j++) {
+      if (!hash[routes[i][j]]) {
+        hash[routes[i][j]] = [i];
+      } else {
+        hash[routes[i][j]].push(i);
+      }
+    }
+  }
+
+  // console.log( typeof hash[1]);
+
+  queue.push(source);
+
+  while (queue.length) {
+    let len = queue.length;
+
+    for (let i = 0; i < len; i++) {
+      let station = queue.shift();
+
+      for (let line of hash[station]) {
+        if (visited[line] === 0) {
+          visited[line] = 1;
+          for (let bus of routes[line]) {
+            if (bus === target) return (L += 1);
+            queue.push(bus);
+          }
+        }
+      }
+    }
+    L++;
+  }
+
+  return answer;
+};
