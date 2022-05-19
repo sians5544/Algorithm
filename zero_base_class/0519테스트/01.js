62ce7935d32c71867dc336f33135cd852b5253e3
sians5544 <tocic@naver.com>
2022-05-19 23:43:58 +0900

    0519 test 2 solved

---
+++ zero_base_class/0519테스트/01.js
@@ -0,0 +1,18 @@
+function solution(s, n) {
+  let answer = 0;
+  let cnt = 0;
+  let map = {};
+  const re = /[A-Z]/;
+  let str = s.split(' ').join('');
+
+  for (let i = 0; i < str.length; i++) {
+    if (!map[str[i]]) {
+      re.test(str[i]) ? (n -= 2) : (n -= 1);
+      map[str[i]] = 1;
+    }
+  }
+
+  return answer > -1 ? s.length : -1;
+}
+
+console.log(solution('time to time', 5));
---
+++ zero_base_class/0519테스트/02.js
@@ -0,0 +1,68 @@
+const { takeCoverage } = require('v8');
+
+function solution(M, remote_tasks, office_tasks, employees) {
+  let answer = [];
+  let offisCheck = Array(M + 1).fill(0); // 사무실 출근자 카운트하기
+  let remoteEmployee = {}; // 리모트 하는 직원들을 가지고 있는 MAP
+  let remoteObj = {};
+
+  remote_tasks.forEach((remote) => {
+    remoteObj[remote] = 1;
+  });
+
+  for (let j = employees.length - 1; j >= 0; j--) {
+    let task = employees[j].split(' ');
+    let team = task.shift();
+
+    if (task.filter((t) => remoteObj[t]).length === task.length) {
+      if (!remoteEmployee[team]) remoteEmployee[team] = [];
+      remoteEmployee[team].push(j + 1);
+      continue;
+    }
+    offisCheck[team]++;
+  }
+
+  offisCheck.forEach((check, index) => {
+    if (index !== 0 && check === 0) {
+      remoteEmployee[index].pop();
+    }
+    if (index !== 0) answer.push(...remoteEmployee[index]);
+  });
+
+  return answer.sort((a, b) => a - b);
+}
+
+console.log(
+  solution(
+    3,
+    ['develoment', 'marketing', 'hometask'],
+    ['recruitment', 'education', 'officetask'],
+    [
+      '1 develoment hometask',
+      '1 recruitment marketing',
+      '2 hometask',
+      '2 develoment marketing hometask',
+      '3 marketing',
+      '3 officetask',
+      '3 develoment',
+    ]
+  )
+); //[1, 4, 5, 7]
+
+console.log(
+  solution(
+    2,
+    ['design'],
+    ['building', 'supervise'],
+    ['2 design', '1 supervise building design', '1 design', '2 design']
+  )
+);
+
+console.log(
+  solution(
+    2,
+    ['design'],
+    ['building', 'supervise'],
+    ['2 design', '1 supervise building design', '1 design', '2 design']
+  )
+);
