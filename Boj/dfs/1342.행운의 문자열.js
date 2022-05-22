cdf7550cd110ffe9d99c6b784eb6db7033af76f2
sians5544 <tocic@naver.com>
2022-05-22 15:43:38 +0900

    boj 1342 time-saving replay

--- Boj/dfs/1342.행운의 문자열.js
+++ Boj/dfs/1342.행운의 문자열.js
@@ -5,42 +5,31 @@ const { isGeneratorFunction } = require('util/types');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 let input = fs.readFileSync(filePath).toString().trim().split('\n');
 let str = input[0].trim().split('');
-
-let tmp = [];
 let len = str.length;
-let answer = 0;
 let set = new Set();
-let check = Array(len).fill(0);
-set.add(str.join(''));
 
-const DFS = (L) => {
-  if (L === len && !set.has(tmp.slice().join(''))) {
-    set.add(tmp.slice().join(''));
+const DFS = (L, tmp, check) => {
+  if (tmp.length === len) {
+    set.add(tmp.join(''));
   } else {
     for (let i = 0; i < len; i++) {
       if (check[i] === 0 && tmp[tmp.length - 1] !== str[i]) {
         check[i] = 1;
         tmp.push(str[i]);
-        DFS(L + 1);
-        tmp.pop();
+        DFS(L + 1, tmp, check);
         check[i] = 0;
+        tmp.pop();
       }
     }
   }
 };
 
-DFS(0, 0);
-
-let arr = [...set];
-
-for (let i = 0; i < arr.length; i++) {
-  let flag = true;
-  for (let j = 1; j < len - 1; j++) {
-    if (!flag) break;
-    if (arr[i][j - 1] === arr[i][j] || arr[i][j + 1] === arr[i][j]) flag = false;
-  }
-
-  if (flag) answer++;
+for (let i = 0; i < len; i++) {
+  let tmp = [];
+  let check = Array(len).fill(0);
+  check[i] = 1;
+  tmp.push(str[i]);
+  DFS(0, tmp, check);
 }
 
-console.log(answer);
+console.log(set.size);
