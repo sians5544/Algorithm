71d5ec23b7f81ec116249faa9e24d73db7a21aa8
sians5544 <tocic@naver.com>
2023-02-20 22:31:20 +0900

    programers alone play solved

---
+++ studyProgrammers/혼자놀기의달인.js
@@ -0,0 +1,12 @@
+function solution(cards) {
+  let answer = 0;
+  let arr = [];
+  let check = [];
+
+  for (let i = 0; i < cards.length; i++) {
+    check = Array(cards.length + 1).fill(0); 
+  }
+  return answer;
+}
+
+console.log(solution([8, 6, 3, 7, 2, 5, 1, 4]));
