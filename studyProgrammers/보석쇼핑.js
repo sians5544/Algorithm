b84a237df2c2c89aa853f93623326eac526a8eb5
Park sian <52738906+sians5544@users.noreply.github.com>
2023-04-25 23:42:24 +0900

    commit test

--- studyProgrammers/보석쇼핑.js
+++ studyProgrammers/보석쇼핑.js
@@ -24,4 +24,3 @@ function solution(gems) {
 console.log(solution(['DIA', 'RUBY', 'RUBY', 'DIA', 'DIA', 'EMERALD', 'SAPPHIRE', 'DIA']));
 console.log(solution(['AA', 'AB', 'AC', 'AA', 'AC']));
 console.log(solution(['XYZ', 'XYZ', 'XYZ']));
-console.log(solution(['ZZZ', 'YYY', 'NNNN', 'YYY', 'BBB']));
