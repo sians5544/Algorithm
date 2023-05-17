c39230ea71999ecaf9272edd2824620826316e39
Park sian <52738906+sians5544@users.noreply.github.com>
2023-05-18 00:39:16 +0900

    programmers 푸드파이트대회 solved

---
+++ studyProgrammers/푸드파이트대회.js
@@ -0,0 +1,8 @@
+function solution(food) {
+    let answer = '';
+    
+    for(let index = 1; index < food.length; index++){
+        answer+=`${index}`.repeat(Math.floor(food[index] / 2));
+    }
+    return `${answer}0${[...answer].reverse().join('')}`;
+}
