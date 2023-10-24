2961416990a03674b5e4280f452d9db720510018
Park sian <52738906+sians5544@users.noreply.github.com>
2023-10-24 23:56:10 +0900

    git token test

---
+++ Boj/greedy/1105.팔.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const { isGeneratorFunction } = require('util/types');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().trim().split('\n');
+
+let [L, R] = input[0].split(' ').map(Number);
+
